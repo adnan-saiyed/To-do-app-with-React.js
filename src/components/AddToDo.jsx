a23fdc455f042f639d372112d7e0c47217a41048
Adnan <adnansaiyed6057@gmail.com>
2024-06-22 19:36:09 +0530

    tttt

--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -1,11 +1,10 @@
 import React ,{useContext, useState} from "react"
-import App from "../App"
 import { AppContext } from "../Context"
 import RemoveAll from "./RemoveAll"
 const AddToDo = () => {
     const [task,setTask] = useState('')
     const {manageTask} = useContext(AppContext)
-    const {tasks} = useContext(AppContext)
+
     
     const objMaker = () => {
 
@@ -13,21 +12,23 @@ const AddToDo = () => {
             task,
             id: Math.random()
         }
-        
         manageTask('ADD_TASK',{newTask: obj})
+        setTask('')
         
     }
     return (
         <>  
             <h2 className="title">To do app</h2>
                 <div className="add-to-do">
-                <input type="text" placeholder="Enter your task here.." value={task}className="text-area"onChange={e => {setTask(e.target.value)}}></input>
-                <span><button className="btn" onClick={objMaker}>+</button></span>
-                <AppContext.Provider value={{manageTask}}>
-                    <RemoveAll/>
-                </AppContext.Provider>
-                
-            </div>
+                    <div className="input">
+                        <input type="text" placeholder="Enter your task here.." value={task} className="text-area" onChange={e => {setTask(e.target.value)}}/>
+                        <button className="btn" onClick={objMaker}>+</button>
+                    </div>
+                    
+                    <AppContext.Provider value={{manageTask}}>
+                        <RemoveAll/>
+                    </AppContext.Provider>
+                </div>
         </>
         
     )
